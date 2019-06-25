fb3ab67011a955e3358cf623f2addc05c41c1ebc
Nick Aversano <nick23hi@gmail.com>
2019-06-25 16:52:19 -0400

    finish ascii render, start justify content

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,22 @@
 enum NodeType { NodeType_DIV, NodeType_TEXT, NodeType_COUNT };
 
 enum PropType {
-  PropValue_FLEX_DIRECTION,
-  PropValue_JUSTIFY_CONTENT,
+  PropType_FLEX_DIRECTION,
+  PropType_JUSTIFY_CONTENT,
   PropType_COUNT
 };
 
 #define FLEX_DIR_ROW 0
 #define FLEX_DIR_COLUMN 1
-
-struct Prop {
-  PropType key;
-  int value;
-};
+#define JUSTIFY_CONTENT_NORMAL 0
+#define JUSTIFY_CONTENT_CENTER 1
+#define JUSTIFY_CONTENT_FLEX_END 2
 
 #define Max(a, b) (a > b ? a : b)
 
 struct Node {
   NodeType type;
-  Prop props[PropType_COUNT];
+  int props[PropType_COUNT];
 
   Node *children;
   int num_children;
@@ -47,17 +45,21 @@ Node makeDivNode() {
   };
 }
 
+void setJustifyContent(Node *node, int value) {
+  node->props[PropType_JUSTIFY_CONTENT] = value;
+}
+
 void printNode(Node *node, int indent = 0) {
   for (int i = 0; i < indent; i++) {
     printf(" ");
   }
 
   if (node->type == NodeType_TEXT) {
-    printf("TextNode: %s - %d %d %d %d", node->text, node->x,
-           node->y, node->width, node->height);
-  } else {
-    printf("DivNode: %d %d %d %d\n", node->x, node->y,
+    printf("TextNode: %s - %d %d %d %d", node->text, node->x, node->y,
            node->width, node->height);
+  } else {
+    printf("DivNode: %d %d %d %d\n", node->x, node->y, node->width,
+           node->height);
 
     for (int i = 0; i < node->num_children; i++) {
       printNode(&node->children[i], indent + 2);
@@ -75,23 +77,56 @@ MEASURE_TEXT(monoSquareMeasure) {
   *y = 1;
 }
 
+int getTotalWidth(Node *nodes, int n) {
+  int total = 0;
+  for (int i = 0; i < n; i++) {
+    total += nodes[i].width;
+  }
+  return total;
+}
+
+int calcChildrenOffset(int justifyContentValue, int childrenWidth,
+                       int parentWidth) {
+  switch (justifyContentValue) {
+  case JUSTIFY_CONTENT_CENTER:
+    return (parentWidth - childrenWidth) / 2;
+  case JUSTIFY_CONTENT_FLEX_END:
+    return parentWidth - childrenWidth;
+  case JUSTIFY_CONTENT_NORMAL:
+  default:
+    return 0;
+  }
+}
+
 void layoutNodes(Node *node, MeasureTextFn *measureText) {
+
   if (node->type == NodeType_TEXT) {
     measureText(0, node->text, &node->width, &node->height);
-  } else if (node->type == NodeType_DIV) {
+    return;
+  }
+
+  if (node->type == NodeType_DIV) {
+    // compute dimensions on each node
     for (int i = 0; i < node->num_children; i++) {
       Node *child = &node->children[i];
       layoutNodes(child, measureText);
-      node->width += child->width;
       node->height = Max(node->height, child->height);
     }
 
-    int xPos = 0;
+    // compute total width of children
+    int childrenWidth = getTotalWidth(node->children, node->num_children);
+    node->width = Max(node->width, childrenWidth);
+
+    // update final x position of children
+    int xPos = calcChildrenOffset(node->props[PropType_JUSTIFY_CONTENT],
+                                  childrenWidth, node->width);
+
     for (int i = 0; i < node->num_children; i++) {
       Node *child = &node->children[i];
       child->x = xPos;
       xPos += child->width;
     }
+    return;
   }
 }
 
@@ -100,7 +135,7 @@ void asciiRenderNode(Node *node, char *output, int width, int height) {
   int index = node->y * width + node->x;
 
   if (node->type == NodeType_TEXT) {
-    strcpy(&output[index], node->text);
+    memcpy(&output[index], node->text, strlen(node->text));
   } else {
     for (int i = 0; i < node->num_children; i++) {
       Node *child = &node->children[i];
@@ -110,18 +145,39 @@ void asciiRenderNode(Node *node, char *output, int width, int height) {
 }
 
 void asciiRender(Node *node) {
-  int buf_len = node->width * node->height + 1;
+  int width = node->width;
+  int height = node->height;
+
+  int buf_len = width * height + 1;
   char *buffer = (char *)malloc(sizeof(char) * (buf_len));
+  memset(buffer, ' ', buf_len);
   buffer[buf_len] = '\0';
 
-  asciiRenderNode(node, buffer, node->width, node->height);
+  asciiRenderNode(node, buffer, width, height);
 
-  printf("%s", buffer);
+  printf("width: %d\nheight: %d\n", width, height);
+
+  // print
+  for (int i = 0; i < width; i++)
+    printf("-");
+  printf("\n");
+
+  for (int i = 0; i < height; i++) {
+    fwrite(&buffer[i * width], 1, width, stdout);
+    printf("\n");
+  }
+
+  for (int i = 0; i < width; i++)
+    printf("-");
+
   free(buffer);
 }
 
 int main() {
   Node div = makeDivNode();
+  div.height = 12;
+  div.width = 32;
+  setJustifyContent(&div, JUSTIFY_CONTENT_CENTER);
   Node children[2] = {makeTextNode("Hello "), makeTextNode("world!")};
   div.children = children;
   div.num_children = 2;
